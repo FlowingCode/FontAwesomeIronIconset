b91a5367e7f42a4ad78964cb9ff13b3cc08b0d5a
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-02-02 16:15:38 -0300

    chore: update license headers

--- src/main/resources/META-INF/resources/frontend/font-awesome-duotone/duotone.js
+++ src/main/resources/META-INF/resources/frontend/font-awesome-duotone/duotone.js
@@ -1,6 +1,6 @@
 /*@license
   FontAwesome Iron Iconset
-  Copyright (C) 2019 - 2020 Flowing Code
+  Copyright (C) 2019 - 2022 Flowing Code
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
