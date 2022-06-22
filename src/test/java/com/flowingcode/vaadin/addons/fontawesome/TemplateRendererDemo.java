a8ff2faac9918fe85386b22e686b4848db086489
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-06-22 14:26:06 -0300

    refactor(test): remove unused methods

--- src/test/java/com/flowingcode/vaadin/addons/fontawesome/TemplateRendererDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/fontawesome/TemplateRendererDemo.java
@@ -66,20 +66,11 @@ public class TemplateRendererDemo extends VerticalLayout {
 		public int getId() {
 			return id;
 		}
-		public void setId(int id) {
-			this.id = id;
-		}
 		public String getName() {
 			return name;
 		}
-		public void setFirstName(String name) {
-			this.name = name;
-		}
 		public int getAge() {
 			return age;
-		}
-		public void setAge(int age) {
-			this.age = age;
 		}
 	}
 }
